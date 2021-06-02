3748713e28287bbe80f551b559ebe543afa0b943
Leah1977 <lh020277@gmail.com>
2021-06-02 15:01:27 +0000

    create empty functions for game

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ function moveCounter() {
 
   } else {
     second = 0;
+    stopTimer();
     return;
   }
 }
@@ -296,4 +297,154 @@ setInterval(function() {
 //     }
 
 //     let gamecomplete = true;
+
+
+// code tried with classes and constructors
+// unable to connect it with browser
+
+
+// class fishyPairs {
+//   constructor(totalTime, cards) {
+    
+//     this.cardsArray = cards;       /
+//     this.totalTime = totalTime;
+//     this.timeRemaining = totalTime;
+//     this.timer = document.getElementById('timer');
+//     this.moves = document.getElementById('moves');
+//     this.restart = document.getElementById('restart');
+//   }
+
+
+//   startGame() {
+//     this.totalClicks = 0;
+//     this.timeRemaining = this.totalTime; //time will reset each new game
+//     this.cardToCheck = null;
+//     this.matchedCards = [];
+//     this.busy = true;
+//     setTimeout (() => {
+//       this.shuffle(this.cardsArray);
+//       this.countDown = this.startCountDown();
+//       this.busy = false;
+//     }, 500);   // half a second time-out 
+//     this.hideCards();
+//     this.timer.innerHTML = this.timeRemaining;
+//     this.moves.innerHTML = this.totalClicks;
+   
+//   }
+
+//   hideCards() {
+//     this.cardsArray.forEach(cards => {
+//       cards.classList.remove('visible');
+//       cards.classList.remove('matched');
+//     });
+//   }
+
+//   flipCard(card) {
+//     if(this.canFlipCard(card)) {
+//       this.totalClicks++;
+//       this.moves.innerHTML = this.totalClicks;  
+//       cards.classList.add('visible');
+
+//       if(this.cardToCheck) {
+//         this.checkForCardMatch(card);
+//       } else {
+//         this.cardToCheck = card;
+      
+//       }
+//     }
+
+//     checkForCardMatch(card) {
+//       if(this.getCardType(card) === this.getCardType(this.cardToCheck));
+//            this.cardMatch(card, this.cardToCheck);
+//       else
+//            this.cardMismatch(card, this.cardToCheck);
+  
+//       this.cardToCheck = null;  
+//     }
+
+//     cardMatch(card1, card2) {
+//       this.matchedCards.push(card1);
+//       this.matchedCards.push(card2);
+//       card1.classList.add('matched');
+//       if(this.matchedCards.length === this.cardsArray.length);
+//          this.winner();
+//     }
+  
+//     cardMismatch(card1, card2) {    //give the player a second to see the image
+//       this.busy = true;
+//       setTimeout(() => {
+//         card1.classList.remove('visible');
+//         card2.classList.remove('visible');
+//         this.busy= false;
+//       }, 1000);
+//     }
+
+//     getCardType(card) {
+//       return card.getElementsByClassName('card-front-face')[0].src;
+//     }
+
+//     startCountDown() {
+//       return setInterval(() => {
+//         this.timeRemaining--;
+//         this.timer.innerHTML = this.timeRemaining;
+//         if (this.timeRemaining === 0)
+//              this.gameOver();
+//       }, 1000);
+//     }
+
+//     gameOver() {
+//       clearInterval(this.countDown);
+//       document.getElementById('game-over').classList.add('visible');
+//     }
+
+//     winner() {
+//       clearInterval(this.countDown);
+//       document.getElementById('winner').classList.add('visible');
+//       this.hideCards();
+//     }
+  
+
+//     // Used Fisher-Yates Shuffle Algorithm Wikipedia
+//   shuffle(cardsArray) {   
+//     for (let i = this.cardsArray.length - 1; 1> 0; i --) {
+//       let randomIndex = Math.floor(Math.random() * (i + 1));   //creates a random float number which we add 1.
+//       cardsArray[randomIndex].style.order = i;
+//       cardsArray[i].style.order = randomIndex;
+//     }
+//   }
+
+//   canFlipCard(card) {
+
+//     return !this.busy && !this.matchedCards.includes(card) && card !==
+//     this.cardToCheck;
+//     }
+//   }
+
+// function ready() {
+//   let overlays = Array.from(document.getElementsByClassName('overlay-text')); 
+//   //to create an array from the html collection
+//   let cards = Array.from(document.getElementsByClassName('cards'));   
+//   //to create an array from the element
+//   let game = new fishyPairs(30, cards);
+
+//   overlays.forEach(overlay => {
+//      overlay.addEventListener('click', () => {
+//        overlay.classList.remove('visible');
+//        game.startGame();
+//      });
+//   });
+//   cards.forEach(card => {
+//     card.addEventListener('click', () => {
+//       game.flipCard(card);
+//     });
+//   });
+//  } 
+
+//  if(document.readyState === 'loading') {              
+//   // if the page hasn't loaded display "loading"
+//     document.addEventListener('DOMContentLoaded', ready());  
+// } else {
+//     ready();
+// }
+
 
