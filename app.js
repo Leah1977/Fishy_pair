67e292fe2383740f0b354409deeed1eeeff2f120
Leah1977 <lh020277@gmail.com>
2021-06-04 11:31:21 +0000

    add comments to javaScript to outline flow of game

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+// 1. Choose a level to play.
+    // 1.1 Create an addEventListener to Play button.
+
+    // Declare Variable for level choice
+
 const cards = document.querySelectorAll('.cards');
 
 let hasFlippedCard = false;
 let lockGameboard = false;
 let firstCard, secondCard;
 let cardsFlipped = 0;
+let button = onclick;
+let startGame;
 
 
-const sound = document.getElementById("gamesound");
 
-sound.muted = true;  // mute sound
+//       //   1.1.1. Go to game resetBoard
+
 
 
 
@@ -21,7 +28,7 @@ function flipCard() {
     
     if(!hasFlippedCard) {
         //first card clicked
-        hasFlippedCard = true
+        hasFlippedCard = true;
         firstCard = this
 
         return;
@@ -33,7 +40,50 @@ function flipCard() {
         checkIfMatch()
 }
 
+
+
+//       //   1.1.2. Timer resets to 60 seconds.
+//       //   1.1.2. Moves resets to 0.
+
+// // 2. Start Game with first card choice.
+//      // 2.1. Choose a card to check
+//         // 2.1.1. Add click event to cards.
+//         // 2.1.2. Click on first card.
+//         // 2.1.3 Flip card to rotate.
+//         // 2.1.3 Choose a second card.
+        
+
+// // 3. Check for match.
+//      // 3.1 Compare the two flipped cards.
+
+// // 4. Cards match
+//         // 4.1 If a match cards will remain open.
+//             // 4.1.1. Continue play.
+
+// // 5. Cards no match
+//       // 5.1 If cards do not match.
+//           // 5.1.1 Add a delay of 1 second.
+//           // 5.1.2 Cards flip back.
+//           // 5.1.3 Choose your next two cards.
+
+// // 6. Continue Play till all the cards match.
+
+// // 7. When all matches have been made.
+//       // 7.1. Game Ends        
+//         // 7.1.1 A pop up of Congratulations will appear if game completed before time runs out.
+//         // 7.1.2 A pop up of Hard Luck will appear if time runs out before all matches are made.
       
+//         // 7.2 Board resets.
+
+// // 8.  Restart Game.
+//        // Player will have an option to play again.
+
+// // 9.  Reset game
+//        // 9.1. Player can choose to reset the board at any time.
+//           // 9.1.1 Game board will reset
+//           // 9.1.2 Timer will reset
+//           // 9.1.3 Moves will reset
+     
 
 function checkIfMatch() {
     //do cards match
@@ -91,7 +141,7 @@ cards.forEach(card => card.addEventListener('click', flipCard));   //flip the ca
 let moves = 0;
 let counter = document.querySelector(".moves");
 
-console.log(moves);
+
 
 // count players moves
 
@@ -115,336 +165,5 @@ function moveCounter() {
 
 // to restart the game with the restart icon
 
-
-
-
-
-
-// level 2 option
-
-function mediumLevel() {
-    let level2 = document.getElementById("pairs-gameboard");
-    level2.innerHTML = `
-      <section id = "pairs-gameboard">
-        <div class = "cards" data-image="smallgoldfish">
-          <img class ="card-front-face" src= "assets/images/jellyfish.gif" alt="smallgoldfish">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>
-        <div class = "cards" data-image="smallgoldfish">
-          <img class ="card-front-face" src= "assets/images/jellyfish.gif" alt="smallgoldfish">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>
-        <div class = "cards" data-image="clownfish">
-          <img class ="card-front-face" src= "assets/images/turtle.gif" alt="clownfish">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>
-        <div class = "cards" data-image="clownfish">
-          <img class ="card-front-face" src= "assets/images/turtle.gif" alt="clownfish">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>
-        <div class = "cards" data-image="hammerheadshark">
-          <img class ="card-front-face" src= "assets/images/mantis.gif" alt="hammerheadshark">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>
-        <div class = "cards" data-image="hammerheadshark">
-          <img class ="card-front-face" src= "assets/images/mantis.gif" alt="hammerheadshark">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>
-        <div class = "cards" data-image="jellyfish">
-          <img class ="card-front-face" src= "assets/images/findingnemo.jpg" alt="jellyfish">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>
-        <div class = "cards" data-image="jellyfish">
-          <img class ="card-front-face" src= "assets/images/findingnemo.jpg" alt="jellyfish">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>
-        <div class = "cards" data-image="johndory">
-          <img class ="card-front-face" src= "assets/images/nemoanddory.jpg" alt="johndory">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>
-        <div class = "cards" data-image="johndory">
-          <img class ="card-front-face" src= "assets/images/nemoanddory.jpg" alt="johndory">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>
-        <div class = "cards" data-image= "redandbluetail">
-          <img class ="card-front-face" src= "assets/images/shark.gif" alt="redandbluetail">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>
-        <div class = "cards" data-image="redandbluetail">
-          <img class ="card-front-face" src= "assets/images/shark.gif" alt="redandbluetail">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>
-        <div class = "cards" data-image="smallgoldfish">
-          <img class ="card-front-face" src= "assets/images/jellyfish.gif" alt="smallgoldfish">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>
-        <div class = "cards" data-image="smallgoldfish">
-          <img class ="card-front-face" src= "assets/images/jellyfish.gif" alt="smallgoldfish">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-          </div>
-        <div class = "cards" data-image="clownfish">
-          <img class ="card-front-face" src= "assets/images/turtle.gif" alt="clownfish">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>
-        <div class = "cards" data-image="clownfish">
-          <img class ="card-front-face" src= "assets/images/turtle.gif" alt="clownfish">
-          <img class ="card-back-face" src="assets/images/fishpairscardname.png" alt="fish logo">
-        </div>         
-    </section>
-`;
-
-function flipCard() {
-    //flip the card
-    if(lockGameboard) return;
-    if(this === firstCard) return;
-
-    this.classList.toggle('flip');
-    
-    if(!hasFlippedCard) {
-        //first card clicked
-        hasFlippedCard = true
-        firstCard = this
-
-        return;
-    }
-
-        //second card clicked
-        secondCard = this
-
-        checkIfMatch()
-}
-
-      
-
-function checkIfMatch() {
-    //do cards match
-    if(firstCard.dataset.image === secondCard.dataset.image)
-    {
-        //its a match
-        disableCards()
-    }
-    else
-    { 
-        //not a match
-        unflipCards()
-    }
-    
-}
-
-function disableCards() {
-    firstCard.removeEventListener('click', flipCard)
-    secondCard.removeEventListener('click', flipCard)
-
-    resetBoard()
-}
-
-function unflipCards() {
-    lockGameboard = true;
-
-    setTimeout( () => {
-        firstCard.classList.remove('flip')
-        secondCard.classList.remove('flip')
-
-        resetBoard()
-    }, 1400)
-    }
-
-    function resetBoard(){
-        [hasFlippedCard, lockGameboard] = [false, false];
-        [firstCard, secondCard] = [null, null];
-
-    }
-
-    (function shuffle() {
-        cards.forEach(card => {
-            let randomPlace = Math.floor(Math.random() * 16)
-            card.style.order = randomPlace;
-        });
-    })();
-}
-
-
-
-
-
-
-
-// gametimer
-
-setInterval(function() {
-    let timerElement = document.getElementById("timer");
-    let currentTime = timerElement.innerHTML;
-    currentTime --;
-    timerElement.innerHTML = currentTime;
-},1000);
-
-// let remainingTime = 60;
-//     let element = document.getElementById('gametimer');
-//     let timer = setInterval(countdown, 1000); 
-//     let restart = document.getElementsByClassName('pairs-gameboard')
-
-
-//     function countdown() {
-//         if (remainingTime == -1) {
-//             clearTimeout(timer);
-            
-//         } else if {
-//             element.innerHTML = remainingTime + '  seconds remaining';
-//             remainingTime--; 
-//         } 
-        
-        
-//     }
-
-//     let gamecomplete = true;
-
-
-// code tried with classes and constructors
-// unable to connect it with browser
-
-
-// class fishyPairs {
-//   constructor(totalTime, cards) {
-    
-//     this.cardsArray = cards;       /
-//     this.totalTime = totalTime;
-//     this.timeRemaining = totalTime;
-//     this.timer = document.getElementById('timer');
-//     this.moves = document.getElementById('moves');
-//     this.restart = document.getElementById('restart');
-//   }
-
-
-//   startGame() {
-//     this.totalClicks = 0;
-//     this.timeRemaining = this.totalTime; //time will reset each new game
-//     this.cardToCheck = null;
-//     this.matchedCards = [];
-//     this.busy = true;
-//     setTimeout (() => {
-//       this.shuffle(this.cardsArray);
-//       this.countDown = this.startCountDown();
-//       this.busy = false;
-//     }, 500);   // half a second time-out 
-//     this.hideCards();
-//     this.timer.innerHTML = this.timeRemaining;
-//     this.moves.innerHTML = this.totalClicks;
-   
-//   }
-
-//   hideCards() {
-//     this.cardsArray.forEach(cards => {
-//       cards.classList.remove('visible');
-//       cards.classList.remove('matched');
-//     });
-//   }
-
-//   flipCard(card) {
-//     if(this.canFlipCard(card)) {
-//       this.totalClicks++;
-//       this.moves.innerHTML = this.totalClicks;  
-//       cards.classList.add('visible');
-
-//       if(this.cardToCheck) {
-//         this.checkForCardMatch(card);
-//       } else {
-//         this.cardToCheck = card;
-      
-//       }
-//     }
-
-//     checkForCardMatch(card) {
-//       if(this.getCardType(card) === this.getCardType(this.cardToCheck));
-//            this.cardMatch(card, this.cardToCheck);
-//       else
-//            this.cardMismatch(card, this.cardToCheck);
-  
-//       this.cardToCheck = null;  
-//     }
-
-//     cardMatch(card1, card2) {
-//       this.matchedCards.push(card1);
-//       this.matchedCards.push(card2);
-//       card1.classList.add('matched');
-//       if(this.matchedCards.length === this.cardsArray.length);
-//          this.winner();
-//     }
-  
-//     cardMismatch(card1, card2) {    //give the player a second to see the image
-//       this.busy = true;
-//       setTimeout(() => {
-//         card1.classList.remove('visible');
-//         card2.classList.remove('visible');
-//         this.busy= false;
-//       }, 1000);
-//     }
-
-//     getCardType(card) {
-//       return card.getElementsByClassName('card-front-face')[0].src;
-//     }
-
-//     startCountDown() {
-//       return setInterval(() => {
-//         this.timeRemaining--;
-//         this.timer.innerHTML = this.timeRemaining;
-//         if (this.timeRemaining === 0)
-//              this.gameOver();
-//       }, 1000);
-//     }
-
-//     gameOver() {
-//       clearInterval(this.countDown);
-//       document.getElementById('game-over').classList.add('visible');
-//     }
-
-//     winner() {
-//       clearInterval(this.countDown);
-//       document.getElementById('winner').classList.add('visible');
-//       this.hideCards();
-//     }
-  
-
-//     // Used Fisher-Yates Shuffle Algorithm Wikipedia
-//   shuffle(cardsArray) {   
-//     for (let i = this.cardsArray.length - 1; 1> 0; i --) {
-//       let randomIndex = Math.floor(Math.random() * (i + 1));   //creates a random float number which we add 1.
-//       cardsArray[randomIndex].style.order = i;
-//       cardsArray[i].style.order = randomIndex;
-//     }
-//   }
-
-//   canFlipCard(card) {
-
-//     return !this.busy && !this.matchedCards.includes(card) && card !==
-//     this.cardToCheck;
-//     }
-//   }
-
-// function ready() {
-//   let overlays = Array.from(document.getElementsByClassName('overlay-text')); 
-//   //to create an array from the html collection
-//   let cards = Array.from(document.getElementsByClassName('cards'));   
-//   //to create an array from the element
-//   let game = new fishyPairs(30, cards);
-
-//   overlays.forEach(overlay => {
-//      overlay.addEventListener('click', () => {
-//        overlay.classList.remove('visible');
-//        game.startGame();
-//      });
-//   });
-//   cards.forEach(card => {
-//     card.addEventListener('click', () => {
-//       game.flipCard(card);
-//     });
-//   });
-//  } 
-
-//  if(document.readyState === 'loading') {              
-//   // if the page hasn't loaded display "loading"
-//     document.addEventListener('DOMContentLoaded', ready());  
-// } else {
-//     ready();
-// }
 
 
