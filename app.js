7b5e0bfb4adfa4fe342f95dca5f1e4fbaf2741ff
Leah1977 <lh020277@gmail.com>
2021-06-06 17:45:13 +0000

    add comments to javascript to define flow of game

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-// 1. Choose a level to play.
-    // 1.1 Create an addEventListener to Play button.
 
-    // Declare Variable for level choice
+// // wait for the DOM to load before running the game.
+// // get the button elements and add eventListeners to the buttons.
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Create an array
 
 const cards = document.querySelectorAll('.cards');
 
@@ -9,14 +12,6 @@ let hasFlippedCard = false;
 let lockGameboard = false;
 let firstCard, secondCard;
 let cardsFlipped = 0;
-let button = onclick;
-let startGame;
-
-
-
-//       //   1.1.1. Go to game resetBoard
-
-
 
 
 function flipCard() {
@@ -28,7 +23,7 @@ function flipCard() {
     
     if(!hasFlippedCard) {
         //first card clicked
-        hasFlippedCard = true;
+        hasFlippedCard = true
         firstCard = this
 
         return;
@@ -40,50 +35,7 @@ function flipCard() {
         checkIfMatch()
 }
 
-
-
-//       //   1.1.2. Timer resets to 60 seconds.
-//       //   1.1.2. Moves resets to 0.
-
-// // 2. Start Game with first card choice.
-//      // 2.1. Choose a card to check
-//         // 2.1.1. Add click event to cards.
-//         // 2.1.2. Click on first card.
-//         // 2.1.3 Flip card to rotate.
-//         // 2.1.3 Choose a second card.
-        
-
-// // 3. Check for match.
-//      // 3.1 Compare the two flipped cards.
-
-// // 4. Cards match
-//         // 4.1 If a match cards will remain open.
-//             // 4.1.1. Continue play.
-
-// // 5. Cards no match
-//       // 5.1 If cards do not match.
-//           // 5.1.1 Add a delay of 1 second.
-//           // 5.1.2 Cards flip back.
-//           // 5.1.3 Choose your next two cards.
-
-// // 6. Continue Play till all the cards match.
-
-// // 7. When all matches have been made.
-//       // 7.1. Game Ends        
-//         // 7.1.1 A pop up of Congratulations will appear if game completed before time runs out.
-//         // 7.1.2 A pop up of Hard Luck will appear if time runs out before all matches are made.
       
-//         // 7.2 Board resets.
-
-// // 8.  Restart Game.
-//        // Player will have an option to play again.
-
-// // 9.  Reset game
-//        // 9.1. Player can choose to reset the board at any time.
-//           // 9.1.1 Game board will reset
-//           // 9.1.2 Timer will reset
-//           // 9.1.3 Moves will reset
-     
 
 function checkIfMatch() {
     //do cards match
@@ -141,7 +93,7 @@ cards.forEach(card => card.addEventListener('click', flipCard));   //flip the ca
 let moves = 0;
 let counter = document.querySelector(".moves");
 
-
+console.log(moves);
 
 // count players moves
 
@@ -161,9 +113,57 @@ function moveCounter() {
     return;
   }
 }
+})
+// ___________________________________________________
 
 
-// to restart the game with the restart icon
 
 
+    // 1.1 Create an addEventListener to Play button.
+
+    // Declare Variable for level choice
+
+    //   1.1.1. Go to game resetBoard
+
+     //   1.1.2. Timer resets to 60 seconds.
+      //   1.1.2. Moves resets to 0.
+
+// 2. Start Game with first card choice.
+     // 2.1. Choose a card to check
+        // 2.1.1. Add click event to cards.
+        // 2.1.2. Click on first card.
+        // 2.1.3 Flip card to rotate.
+        // 2.1.3 Choose a second card.
+        
+
+// 3. Check for match.
+     // 3.1 Compare the two flipped cards.
+
+// 4. Cards match
+        // 4.1 If a match cards will remain open.
+            // 4.1.1. Continue play.
+
+// 5. Cards no match
+      // 5.1 If cards do not match.
+          // 5.1.1 Add a delay of 1 second.
+          // 5.1.2 Cards flip back.
+          // 5.1.3 Choose your next two cards.
+
+// 6. Continue Play till all the cards match.
+
+// 7. When all matches have been made.
+      // 7.1. Game Ends        
+        // 7.1.1 A pop up of Congratulations will appear if game completed before time runs out.
+        // 7.1.2 A pop up of Hard Luck will appear if time runs out before all matches are made.
+      
+        // 7.2 Board resets.
+
+// 8.  Restart Game.
+       // Player will have an option to play again.
+
+// 9.  Reset game
+       // 9.1. Player can choose to reset the board at any time.
+          // 9.1.1 Game board will reset
+          // 9.1.2 Timer will reset
+          // 9.1.3 Moves will reset
 
